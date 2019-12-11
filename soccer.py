21d17a6c29473bebb1b5f2df56c29961a7720a13
Jorge Rosas <jorgemonday@gmail.com>
2019-12-11 09:13:48 -0800

    Updated code style to conform to pep8 style guidelines

--- soccer.py
+++ soccer.py
@@ -1,19 +1,22 @@
 # -*- coding: utf-8 -*-
 
 # -----------------------------------------------------------
-# This program opens the soccer.dat file located in the same directory, and outputs the name of the team with the 
-# smallest difference between the 'for' and 'against' goals. 
-# 
+# This program opens the soccer.dat file located in the same directory,
+# and outputs the name of the team with the smallest difference
+# between the 'for' and 'against' goals.
+#
 # This program expects a file formatted a follows -
 # File encoding: us-ascii
 # Column 8: 15 character max string respresenting the team name
-# Column 44: 1 to 3 digit number representing the number of goals scored by the team
-# Column 51: 1 to 3 digit number representing the number of goals scored against the team
+# Column 44: 3 digit number representing number of goals scored by team
+# Column 51: 3 digit number representing number of goals scored against team
 #
 # Original assignment text:
-# The attached soccer.dat file contains the results from the English Premier League.  The columns labeled ‘F’ and ‘A’ contain the 
-# total number of goals scored for and against each team in that season (so Arsenal scored 79 goals against opponents, and had 
-# 36 goals scored against them). Write a program to print the name of the team with the smallest difference in ‘for’ 
+# The attached soccer.dat file contains the results from the English Premier
+# League.  The columns labeled ‘F’ and ‘A’ contain the total number of goals
+# scored for and against each team in that season (so Arsenal scored 79 goals
+# against opponents, and had 36 goals scored against them). Write a program
+# to print the name of the team with the smallest difference in ‘for’
 # and ‘against’ goals.
 #
 # Jorge Rosas
@@ -26,7 +29,7 @@ import sys
 # Application constants
 MIN_PYTHON_VERSION = (2, 7)
 DATA_FILE_NAME = 'soccer.dat'
-RECORD_MINIMUM_NUM_CHARACTERS = 53 # If less, not a valid record
+RECORD_MINIMUM_NUM_CHARACTERS = 53  # If less, not a valid record
 RECORD_START_POSITION_TEAM_NAME = 7
 RECORD_END_POSITION_TEAM_NAME = 21
 RECORD_START_POSITION_GOALS_FOR = 43
@@ -36,47 +39,56 @@ RECORD_END_POSITION_GOALS_AGAINST = 53
 
 # Require a minimum version of python to guarantee compatability
 if sys.version_info < MIN_PYTHON_VERSION:
-    sys.exit("Sorry, Python %s.%s or later is required.\n" % MIN_PYTHON_VERSION)
+    sys.exit("Sorry, Python %s.%s or later is required.\n" %
+             MIN_PYTHON_VERSION)
 
 # These variables will hold the final result
 result_team = None
 result_spread = None
 
-try :
+try:
     # open the file in read only mode
-    with io.open(DATA_FILE_NAME, mode='r', encoding='us-ascii') as soccer_file :                 
-        
-        lines = [line.rstrip('\n') for line in soccer_file]     
-        
-        for line in lines :
-            
-            if len(line) > RECORD_MINIMUM_NUM_CHARACTERS :
-                
-                try : # if not a valid data record, it will fail and move on to the next line.                    
-                    team_name = line[RECORD_START_POSITION_TEAM_NAME : RECORD_END_POSITION_TEAM_NAME].strip() 
-                    goals_for = int(line[RECORD_START_POSITION_GOALS_FOR : RECORD_END_POSITION_GOALS_FOR].strip())
-                    goals_against = int(line[RECORD_START_POSITION_GOALS_AGAINST : RECORD_END_POSITION_GOALS_AGAINST].strip())
+    with io.open(DATA_FILE_NAME, mode='r', encoding='us-ascii') as soccer_file:
+
+        lines = [line.rstrip('\n') for line in soccer_file]
+
+        for line in lines:
+
+            if len(line) > RECORD_MINIMUM_NUM_CHARACTERS:
+
+                # if not a valid data record, will fail and move to next line.
+                try:
+                    team_name = line[RECORD_START_POSITION_TEAM_NAME:
+                                     RECORD_END_POSITION_TEAM_NAME].strip()
+                    goals_for = int(line[RECORD_START_POSITION_GOALS_FOR:
+                                         RECORD_END_POSITION_GOALS_FOR]
+                                    .strip())
+                    goals_against = int(line[
+                                        RECORD_START_POSITION_GOALS_AGAINST:
+                                        RECORD_END_POSITION_GOALS_AGAINST]
+                                        .strip())
                     goal_spread = goals_for - goals_against
 
-                    if goal_spread < 0 :
-                        goal_spread = goal_spread * -1 # flip to positive if the team had a higher number of goals against them.
+                    # a spread is always a positive number
+                    if goal_spread < 0:
+                        goal_spread = goal_spread * -1
 
-                    if not result_spread :
+                    if not result_spread:
                         result_spread = goal_spread
-                        result_team = team_name                        
-                    else :
+                        result_team = team_name
+                    else:
                         # compare and set a new value if this is a lower spread
-                        if goal_spread < result_spread : 
+                        if goal_spread < result_spread:
                             result_spread = goal_spread
                             result_team = team_name
 
-                except ValueError : # not a valid line for this application
+                except ValueError:  # not a valid line for this application
                     continue
 
     # compute finished, display the team with the lowest spread and quit.
     print(str(result_team))
     quit()
 
-except IOError :
-    print ('Could not read the soccer file, program exiting')
+except IOError:
+    print('Could not read the soccer file, program exiting')
     quit()
--- weather.py
+++ weather.py
@@ -1,21 +1,26 @@
 # -*- coding: utf-8 -*-
 
 # -----------------------------------------------------------
-# This program opens the w_data (5).dat file located in the same directory, and outputs the day number with the
-# smallest spread between the MnT column (lowest temperature of the day) and the MxT column (highest temperature of the day).
+# This program opens the w_data (5).dat file located in the same directory,
+# and outputs the day number with the smallest spread between the MnT column
+# (lowest temperature of the day) and the MxT column (highest temperature
+# of the day).
 #
 # This program expects a file formatted a follows -
 # File encoding: us-ascii
 # Column 3:  1 or 2 digit numerical day number in a range from 1 to 31
-# Column 6:  5 character maximum temperature field, with optional negative first character, optional asterisk denoting monthly high
-# Column 13: 5 character minimum temperature field, with optional negative first character, optional asterisk denoting monthly low
+# Column 6:  5 character max temperature field, with optional negative first
+#            character, optional asterisk denoting monthly high
+# Column 13: 5 character minimum temperature field, with optional negative
+#            first character, optional asterisk denoting monthly low
 #
 # Original assignment text:
-# In the attached file (w_data.dat), you’ll find daily weather data.   Download this text file, then write a program to output the 
-# day number (column one) with the smallest temperature spread (the maximum temperature is the second column, 
-# the minimum the third column).
+# In the attached file (w_data.dat), you’ll find daily weather data.
+# Download this text file, then write a program to output the day number
+# (column one) with the smallest temperature spread (the maximum
+# temperature is the second column, the minimum the third column).
 #
-# 
+#
 # Jorge Rosas
 # https://github.com/jorgemonday
 # -----------------------------------------------------------
@@ -26,7 +31,7 @@ import sys
 # Application constants
 MIN_PYTHON_VERSION = (2, 7)
 DATA_FILE_NAME = 'w_data (5).dat'
-RECORD_MINIMUM_NUM_CHARACTERS = 20 # If less, not a valid record
+RECORD_MINIMUM_NUM_CHARACTERS = 20  # If less, not a valid record
 RECORD_START_POSITION_DAY = 0
 RECORD_END_POSITION_DAY = 5
 RECORD_START_POSITION_LOW_TEMP = 12
@@ -36,45 +41,53 @@ RECORD_END_POSITION_HIGH_TEMP = 11
 
 # Require a minimum version of python to guarantee compatability
 if sys.version_info < MIN_PYTHON_VERSION:
-    sys.exit("Sorry, Python %s.%s or later is required.\n" % MIN_PYTHON_VERSION)
+    sys.exit("Sorry, Python %s.%s or later is required.\n" %
+             MIN_PYTHON_VERSION)
 
 # These variables will hold the final result
 result_day = None
 result_spread = None
 
 
-try :
+try:
     # open the file in read only mode
-    with io.open(DATA_FILE_NAME, mode='r', encoding='us-ascii') as weather_file :                 
-        
-        lines = [line.rstrip('\n') for line in weather_file]     
-        
-        for line in lines :
-            
-            if len(line) > RECORD_MINIMUM_NUM_CHARACTERS : 
-                
-                try : # if not a valid data record, fail and move on to the next line.                    
-                    day_of_month = int(line[RECORD_START_POSITION_DAY : RECORD_END_POSITION_DAY].strip()) 
-                    day_low_temp = int(line[RECORD_START_POSITION_LOW_TEMP : RECORD_END_POSITION_LOW_TEMP].replace('*','').strip())
-                    day_high_temp = int(line[RECORD_START_POSITION_HIGH_TEMP : RECORD_END_POSITION_HIGH_TEMP].replace('*','').strip())
+    with io.open(DATA_FILE_NAME,
+                 mode='r', encoding='us-ascii') as weather_file:
+
+        lines = [line.rstrip('\n') for line in weather_file]
+
+        for line in lines:
+
+            if len(line) > RECORD_MINIMUM_NUM_CHARACTERS:
+
+                # if not a valid data record, fail and move to the next line.
+                try:
+                    day_of_month = int(line[RECORD_START_POSITION_DAY:
+                                            RECORD_END_POSITION_DAY].strip())
+                    day_low_temp = int(line[RECORD_START_POSITION_LOW_TEMP:
+                                            RECORD_END_POSITION_LOW_TEMP].
+                                       replace('*', '').strip())
+                    day_high_temp = int(line[RECORD_START_POSITION_HIGH_TEMP:
+                                             RECORD_END_POSITION_HIGH_TEMP].
+                                        replace('*', '').strip())
                     day_temp_spread = day_high_temp - day_low_temp
 
-                    if not result_spread :
+                    if not result_spread:
                         result_spread = day_temp_spread
                         result_day = day_of_month
-                    else :
+                    else:
                         # compare and set a new value if this is a lower spread
-                        if day_temp_spread < result_spread : 
+                        if day_temp_spread < result_spread:
                             result_spread = day_temp_spread
                             result_day = day_of_month
 
-                except ValueError : # not a valid line for this application
+                except ValueError:  # not a valid line for this application
                     continue
 
-    # compute finished, display the day of the month with lowest spread and quit.
+    # compute finished, display day of the month with lowest spread and quit.
     print(str(result_day))
     quit()
 
-except IOError :
-    print ('Could not read the weather file, program exiting')
+except IOError:
+    print('Could not read the weather file, program exiting')
     quit()
